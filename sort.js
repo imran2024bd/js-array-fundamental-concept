1127b9b7cfc411757d6c71209936833fcc0a1d91
imran2024bd <imranitbd34@gmail.com>
2024-02-02 00:57:35 +0600

    js-problems-part2-practice-tasks 03 problem solve

--- sort.js
+++ sort.js
@@ -13,5 +13,5 @@ const numbers = [4, 7, 12, 8, 43, 6, 1];
 const numbers_asc = [...numbers].sort(function (a, b) { return a - b })
 const numbers_dsc = [...numbers].sort(function (a, b) { return b - a })
 
-console.log(numbers_asc);
-console.log(numbers_dsc);
+// console.log(numbers_asc);
+// console.log(numbers_dsc);
