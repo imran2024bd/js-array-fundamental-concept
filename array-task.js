4ccc38543ceb683a06992e1e177ac381bf182520
imran2024bd <imranitbd34@gmail.com>
2024-02-01 23:12:56 +0600

    Array home task starting

---
+++ array-task.js
@@ -0,0 +1,14 @@
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+const fruits = ["Banana", "Orange", "Apple", "Mango" , 'banana'];
+console.log(fruits[3]);
+fruits[2] = 'jambura';
+console.log(fruits);
+
+// console.log(fruits.indexOf('Apple'));
+// console.log(fruits.indexOf('Mango'));
+
